c9f37aa8765be6113197e7210e7a2421a91e243e
gonzarascon <gonzarascon@gmail.com>
2020-05-30 19:03:56 -0300

    WIP: lecture page + Md renderer

--- components/Markdown/Renderers/index.js
+++ components/Markdown/Renderers/index.js
@@ -1,9 +1,11 @@
 import code from './code';
 import inlineCode from './inlineCode';
 import heading from './heading';
+import link from './link';
 
 export default {
   code,
   inlineCode,
+  link,
   heading,
 };
